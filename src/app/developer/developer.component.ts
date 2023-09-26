16caa2e07c9e7f42f811ef5248d7f75f1c9636ee
nefzina <nefzi.amani@gmail.com>
2023-09-26 21:12:39 +0200

    deleted comments

--- src/app/developer/developer.component.ts
+++ src/app/developer/developer.component.ts
@@ -10,16 +10,12 @@ import { Skill } from '../models/skill.model';
 
 export class DeveloperComponent {
   developer: Developer;
-  // developer2: Developer;
-
 
   constructor() {
     const skill1 = new Skill("Angular", "../../assets/Angular_full_color_logo.svg.png", "https://angular.io/");
     const skill2 = new Skill("React", "../../assets/React-icon.svg.png", "https://react.dev/");
     const skill3 = new Skill("MySQL", "../../assets/mysql.png", "https://www.mysql.com/");
 
-
     this.developer = new Developer("Doe", "John", 28, "male", "biography 1", [skill1, skill2, skill3])
-    // this.developer2 = new Developer("Doe", "Jane", 25, "female", "biography 2", [skill1, skill2])
   }
 }
